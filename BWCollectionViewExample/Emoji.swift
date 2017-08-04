f1b50ea67a26f37c3201f6f432a56818ecaf4dc8
Bruno Fulber Wide <bfwide07@gmail.com>
2017-08-03 22:11:36 -0300

    initial commit

---
+++ BWCollectionViewExample/Emoji.swift
@@ -0,0 +1,20 @@
+//
+//  Emoji.swift
+//  SKCollectionViewExample
+//
+//  Created by Bruno Fulber Wide on 31/07/17.
+//  Copyright © 2017 BW. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class Emoji {
+    var name : String
+    var image : UIImage
+    
+    init(_ name: String, image: UIImage) {
+        self.name = name
+        self.image = image
+    }
+}
---
+++ BWCollectionViewExample/EmojiItem.swift
@@ -0,0 +1,27 @@
+//
+//  emojiItem.swift
+//  SKCollectionViewExample
+//
+//  Created by Bruno Fulber Wide on 31/07/17.
+//  Copyright © 2017 BW. All rights reserved.
+//
+
+import Foundation
+import SpriteKit
+
+class EmojiItem : BWCollectionViewItem {
+    private var nameLabel : SKLabelNode = SKLabelNode()
+    private var imageNode : SKSpriteNode!
+    
+    var emoji : Emoji! {
+        didSet{
+            nameLabel.text = emoji.name
+            imageNode = SKSpriteNode(texture: SKTexture(image: emoji.image))
+            
+            nameLabel.position.y += 80
+            
+            addChild(nameLabel)
+            addChild(imageNode)
+        }
+    }
+}
